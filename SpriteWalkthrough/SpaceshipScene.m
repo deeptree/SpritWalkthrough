3154b30c0fd0789955cae3af892976e7aebb545a
deeptree <willieding@gmail.com>
2013-10-17 15:40:03 +0800

    first milestone done!

--- SpriteWalkthrough/SpaceshipScene.m
+++ SpriteWalkthrough/SpaceshipScene.m
@@ -1,9 +1,9 @@
 //
-//  SpaceshipScene.m
-//  SpriteWalkthrough
+// SpaceshipScene.m
+// SpriteWalkthrough
 //
-//  Created by deeptree on 2013/10/17.
-//  Copyright (c) 2013年 deeptree. All rights reserved.
+// Created by deeptree on 2013/10/17.
+// Copyright (c) 2013年 deeptree. All rights reserved.
 //
 
 #import "SpaceshipScene.h"
@@ -14,19 +14,108 @@
 
 @implementation SpaceshipScene
 
-- (void)didMoveToView:(SKView *)view
+- (void) didMoveToView:(SKView *)view
 {
-    if (!self.contentCreated)
-    {
-        [self createSceneContents];
-        self.contentCreated = YES;
-    }
+	if (!self.contentCreated) {
+		[self createSceneContents];
+		self.contentCreated = YES;
+	}
 }
 
-- (void)createSceneContents
+- (void) createSceneContents
 {
-    self.backgroundColor = [SKColor blackColor];
-    self.scaleMode = SKSceneScaleModeAspectFit;
+	self.backgroundColor = [SKColor blackColor];
+	self.scaleMode = SKSceneScaleModeAspectFit;
+
+	SKSpriteNode *spaceship = [self newSpaceship];
+
+	spaceship.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) - 150);
+	[self addChild:spaceship];
+
+	SKAction *makeRocks = [SKAction sequence:@[
+	                               [SKAction performSelector:@selector(addRock) onTarget:self],
+	                               [SKAction waitForDuration:0.10 withRange:0.15]
+	                       ]];
+
+	[self runAction:[SKAction repeatActionForever:makeRocks]];
+}
+
+- (SKSpriteNode *) newSpaceship
+{
+//	SKSpriteNode *hull = [[SKSpriteNode alloc] initWithColor:[SKColor grayColor] size:CGSizeMake(64, 32)];
+	SKSpriteNode *hull = [[SKSpriteNode alloc] initWithColor:[SKColor grayColor] size:CGSizeMake(300, 32)];
+//	SKAction *hover = [SKAction sequence:@[
+//	                           [SKAction waitForDuration:1.0],
+//	                           [SKAction moveByX:100 y:50.0 duration:1.0],
+//                               [SKAction rotateByAngle:-360.0 duration:1.0],
+//	                           [SKAction waitForDuration:1.0],
+//	                           [SKAction moveByX:-100.0 y:-50 duration:1.0]]];
+	SKAction *hover = [SKAction sequence:@[
+                                           [SKAction waitForDuration:1.0],
+                                           [SKAction rotateByAngle:180.0 duration:1.0],
+                                           [SKAction waitForDuration:1.0]
+                                        ]];
+	[hull runAction:[SKAction repeatActionForever:hover]];
+
+	// add light node to spaceship
+	SKSpriteNode *light1 = [self newLight];
+
+	light1.position = CGPointMake(-28.0, 6.0);
+	[hull addChild:light1];
+
+	SKSpriteNode *light2 = [self newLight];
+
+	light2.position = CGPointMake(28.0, 6.0);
+	[hull addChild:light2];
+
+	// add physics simulation
+	hull.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:hull.size];
+	hull.physicsBody.dynamic = NO;
+
+	return hull;
+}
+
+- (SKSpriteNode *) newLight
+{
+	SKSpriteNode *light = [[SKSpriteNode alloc] initWithColor:[SKColor yellowColor] size:CGSizeMake(8, 8)];
+
+	SKAction *blink = [SKAction sequence:@[
+	                           [SKAction fadeOutWithDuration:0.25],
+	                           [SKAction fadeInWithDuration:0.25]]];
+	SKAction *blinkForever = [SKAction repeatActionForever:blink];
+
+	[light runAction:blinkForever];
+
+	return light;
+}
+
+static inline CGFloat skRandf()
+{
+	return rand() / (CGFloat) RAND_MAX;
+}
+
+static inline CGFloat skRand(CGFloat low, CGFloat high)
+{
+	return skRandf() * (high - low) + low;
+}
+
+- (void) addRock
+{
+	SKSpriteNode *rock = [[SKSpriteNode alloc] initWithColor:[SKColor brownColor] size:CGSizeMake(8, 8)];
+
+	rock.position = CGPointMake(skRand(0, self.size.width), self.size.height - 50);
+	rock.name = @"rock";
+	rock.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:rock.size];
+	rock.physicsBody.usesPreciseCollisionDetection = YES;
+	[self addChild:rock];
+}
+
+-(void)didSimulatePhysics
+{
+    [self enumerateChildNodesWithName:@"rock" usingBlock:^(SKNode *node, BOOL *stop) {
+        if (node.position.y < 0)
+            [node removeFromParent];
+    }];
 }
 
 @end
